f8493dc25d9f3f5948b6d43f9c8ed07463e7a04d
Njoroge Charles <njorodev1319@gmail.com>
2024-07-08 13:06:03 +0300

    securing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
 const productsRoute = require("./routes/product.route.js");
 const app = express();
-
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 //connecting to the database
 mongoose
   .connect(
-    "mongodb+srv://njorodev1319:1319Rip%40%23@backend.fbslqkv.mongodb.net/NodeDB?retryWrites=true&w=majority&appName=backend"
+    `mongodb+srv://njorodev1319:${process.env.DB_PASSWORD}@backend.fbslqkv.mongodb.net/NodeDB?retryWrites=true&w=majority&appName=backend`
   )
   .then(() => {
     console.log("Connected to database");
